ce60c7ea51a1dd6dd5755953ef7b8b9427b8ae50
Daiane Bushey <daianebushey@gmail.com>
2018-03-23 22:47:50 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,9 +16,9 @@ class Doctor
   end
 
   def patients
-    self.appointments.collect |appointment|
+    self.appointments.collect do |appointment|
       appointment.patient
     end
   end
-end
+
 end
