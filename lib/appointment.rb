f5b1fd9557e0268a79ebeebec092b8ca5fe08029
Daiane Bushey <daianebushey@gmail.com>
2018-03-23 22:51:25 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,8 +4,7 @@ class Appointment
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
-    #genre.add_song(self)
     #doctor.add_appointment(self)
-    #patient.add_appointment(self)
+    patient.add_appointment(self)
   end
 end
